9c28377deb93c9d39727d9cb5c62df19824be718
JoseCarlos <jocdiazmuic@gmail.com>
2022-02-03 11:05:43 -0500

    api controller updated

--- api/upload/index.js
+++ api/upload/index.js
@@ -1,11 +1,17 @@
-const { Router }= require('express')
-const multer = require('multer')
-const { uploadSingleHandler, uploadArrayHandler } = require('./upload.controller')
+const { Router } = require('express');
+const multer = require('multer');
+const {
+  uploadSingleHandler,
+  uploadArrayHandler,
+} = require('./upload.controller');
 
-const router = Router()
-const upload = multer({ dest:'./temp'})
+const router = Router();
+const upload = multer({ dest: './temp' });
 
-router.post('/file', upload.single('imageMain'), uploadSingleHandler )
-router.post('/files', upload.any(), uploadArrayHandler )
+router.post('/file', upload.single('imageMain'), uploadSingleHandler);
+router.post('/product', upload.single('image'), uploadSingleHandler);
+router.post('/products', upload.any(), uploadArrayHandler);
+router.post('/market', upload.single('image'), uploadSingleHandler);
+router.post('/avatar', upload.single('picture'), uploadSingleHandler);
 
-module.exports = router
+module.exports = router;
--- api/upload/upload.controller.js
+++ api/upload/upload.controller.js
@@ -1,33 +1,40 @@
-const fs = require('fs')
-const cloudinary = require('cloudinary').v2
+const fs = require('fs');
+const cloudinary = require('cloudinary').v2;
 
 async function uploadSingleHandler(req, res) {
-  const { file } = req
-  try{
-    const result = await cloudinary.uploader.upload(file.path, { folder:'Markt-Pul/Products'})
-    fs.unlinkSync(file.path)
-    res.status(201).json(result)
-  }catch(error){
-    console.log('error', error)
+  console.log(req);
+  const { file, body } = req;
+  const options = {};
+  if (body?.folder) {
+    options.folder = body.folder;
+  }
+  try {
+    const result = await cloudinary.uploader.upload(file.path, options);
+    fs.unlinkSync(file.path);
+    res.status(201).json(result);
+  } catch (error) {
+    console.log('error', error);
   }
 }
 
 async function uploadArrayHandler(req, res) {
-  const { files } = req
-  const results = []
-  for(const file of files){
-    try{
-        const result = await cloudinary.uploader.upload(file.path, { folder:'Markt-Pul/Products'})
-        fs.unlinkSync(file.path)
-        results.push(result)
-    }catch(error){
-      console.log('error', error)
+  const { files } = req;
+  const results = [];
+  for (const file of files) {
+    try {
+      const result = await cloudinary.uploader.upload(file.path, {
+        folder: 'Markt-Pul/Products',
+      });
+      fs.unlinkSync(file.path);
+      results.push(result);
+    } catch (error) {
+      console.log('error', error);
     }
   }
-  return res.status(201).json(results)
+  return res.status(201).json(results);
 }
 
 module.exports = {
   uploadSingleHandler,
-  uploadArrayHandler
-}
+  uploadArrayHandler,
+};
